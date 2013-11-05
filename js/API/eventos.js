59d1c7ff9b448e503213f98893ab6dc40e8d43e4
vvallejo07 <bk_soft@hotmail.com>
2013-11-05 00:56:22 -0600

    sadasdas

--- js/API/eventos.js
+++ js/API/eventos.js
@@ -1,19 +1,6 @@
 //Eventos
 $(document).ready(function(){
 	
-	 var currencies = [
-    { value: 'Afghan afghani', data: 'AFN' },
-    { value: 'Albanian lek', data: 'ALL' },
-    { value: 'Algerian dinar', data: 'DZD' },
-    { value: 'European euro', data: 'EUR' },
-    { value: 'Angolan kwanza', data: 'AOA' },
-    { value: 'East Caribbean dollar', data: 'XCD' },
-    { value: 'Vietnamese dong', data: 'VND' },
-    { value: 'Yemeni rial', data: 'YER' },
-    { value: 'Zambian kwacha', data: 'ZMK' },
-    { value: 'Zimbabwean dollar', data: 'ZWD' },
-  ];
-	
 		//Sincronizar
 		$('#sinc').tap(function(){
 			obtenerRutas();
@@ -27,40 +14,20 @@ $(document).ready(function(){
 			obtenerClientes($( "#bdRuta option:selected" ).text());
 		});
 		
-		$('#autocomplete').autocomplete({
-    lookup: currencies,
-    onSelect: function (suggestion) {
-      var thehtml = '<strong>Currency Name:</strong> ' + suggestion.value + ' <br> <strong>Symbol:</strong> ' + suggestion.data;
-      $('#outputcontent').html(thehtml);
-    }
-  });
-		
-		$('#simplebool').tap(function(){
-		  var dirTxt = $("#bdClient option:selected").attr('dir');
-		  if(dirTxt === "undefined" || dirTxt === ''){
-			  dirTxt = "No cuenta con dirección establecida";
-		  }
-		  
-		  $(this).simpledialog({
-			'mode' : 'bool',
-			'prompt' : dirTxt,
-			'useModal': true,
-			'buttons' : {
-			  'OK': {
-				click: function () {
-				  $('#dialogoutput').text('OK');
-				}
-			  }
-			}
-		  })
-		})
 });
 
 function llenarClientes(clientes){ 
-	$('#bdClient').empty();
+/*	$('#bdClient').empty();
 	for(i=0; i<clientes.length; i++){
 		$('#bdClient').append('<option dir="'+ clientes[i].direccion +'" value="'+ clientes[i].value +'">'+ clientes[i].label +'</option>');
-	}
+	}*/
+		$('#autocomplete').autocomplete({
+		lookup: clientes,
+		onSelect: function (client) {
+		  var thehtml = '<strong>Nombre Comercial:</strong> ' + client.direccion + ' <br> <strong>Geolocalizacion:</strong> ' + client.direccion;
+      $('#outDireccion').html(thehtml);
+		}
+	  });
 }
 
 function sendData(){
