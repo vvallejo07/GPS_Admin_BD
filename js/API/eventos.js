133c4399b5af4522e94e5310531989736f726576
vvallejo07 <bk_soft@hotmail.com>
2013-11-10 12:13:02 -0600

    test8

--- js/API/eventos.js
+++ js/API/eventos.js
@@ -43,7 +43,7 @@ function libReady(){
 }
 
 function onSuccess(pos){
-	alert(pos.coords.latitude);
+	alert( (pos.coords.latitude+'').length );
 	/*if(pos.coords.latitude.length > 13){
 		navigator.notification.alert(pos.coords.latitude + ' .. ' + pos.coords.longitude,null,'Cliente','Aceptar');
 		navigator.geolocation.clearWatch(watchId);
